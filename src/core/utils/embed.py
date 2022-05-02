52dd11786cdd25b6e660b7b6ea971a062e98b5d0
flo <flo@xorhash.com>
2022-05-02 03:38:52 +0100

    Add utils to core

---
+++ src/core/utils/embed.py
@@ -0,0 +1,27 @@
+import random
+
+import discord
+
+
+def randomize_colour(embed: discord.Embed) -> discord.Embed:
+    """
+    Gives the provided embed a random color.
+    There is an alias for this called randomize_color
+
+    Parameters
+    ----------
+    embed : discord.Embed
+        The embed to add a color to
+
+    Returns
+    -------
+    discord.Embed
+        The embed with the color set to a random color
+
+    """
+    embed.colour = discord.Color(value=random.randint(0x000000, 0xFFFFFF))
+    return embed
+
+
+# Fuck you
+randomize_color = randomize_colour
---
+++ src/core/utils/menus.py
@@ -0,0 +1,314 @@
+# Original source of reaction-based menu idea from
+# https://github.com/Lunar-Dust/Dusty-Cogs/blob/master/menu/menu.py
+import asyncio
+import contextlib
+import functools
+import warnings
+from typing import Iterable, List, Optional, Union
+
+import discord
+from discord.ext import commands
+
+from .predicates import ReactionPredicate
+
+_ReactableEmoji = Union[str, discord.Emoji]
+
+# Normal Menu
+async def menu(
+    ctx: commands.Context,
+    pages: Union[List[str], List[discord.Embed]],
+    controls: dict,
+    message: discord.Message = None,
+    page: int = 0,
+    timeout: float = 30.0,
+):
+    """
+    An emoji-based menu
+
+    .. note:: All pages should be of the same type
+
+    .. note:: All functions for handling what a particular emoji does
+              should be coroutines (i.e. :code:`async def`). Additionally,
+              they must take all of the parameters of this function, in
+              addition to a string representing the emoji reacted with.
+              This parameter should be the last one, and none of the
+              parameters in the handling functions are optional
+
+    Parameters
+    ----------
+    ctx: commands.Context
+        The command context
+    pages: `list` of `str` or `discord.Embed`
+        The pages of the menu.
+    controls: dict
+        A mapping of emoji to the function which handles the action for the
+        emoji.
+    message: discord.Message
+        The message representing the menu. Usually :code:`None` when first opening
+        the menu
+    page: int
+        The current page number of the menu
+    timeout: float
+        The time (in seconds) to wait for a reaction
+
+    Raises
+    ------
+    RuntimeError
+        If either of the notes above are violated
+    """
+    if not isinstance(pages[0], (discord.Embed, str)):
+        raise RuntimeError("Pages must be of type discord.Embed or str")
+    if not all(isinstance(x, discord.Embed) for x in pages) and not all(
+        isinstance(x, str) for x in pages
+    ):
+        raise RuntimeError("All pages must be of the same type")
+    for key, value in controls.items():
+        maybe_coro = value
+        if isinstance(value, functools.partial):
+            maybe_coro = value.func
+        if not asyncio.iscoroutinefunction(maybe_coro):
+            raise RuntimeError("Function must be a coroutine")
+    current_page = pages[page]
+
+    if not message:
+        if isinstance(current_page, discord.Embed):
+            message = await ctx.send(embed=current_page)
+        else:
+            message = await ctx.send(current_page)
+        # Don't wait for reactions to be added (GH-1797)
+        # noinspection PyAsyncCall
+        start_adding_reactions(message, controls.keys(), ctx.bot.loop)
+    else:
+        try:
+            if isinstance(current_page, discord.Embed):
+                await message.edit(embed=current_page)
+            else:
+                await message.edit(content=current_page)
+        except discord.NotFound:
+            return
+
+    try:
+        react, user = await ctx.bot.wait_for(
+            "reaction_add",
+            check=ReactionPredicate.with_emojis(
+                tuple(controls.keys()), message, ctx.author
+            ),
+            timeout=timeout,
+        )
+    except asyncio.TimeoutError:
+        try:
+            await message.clear_reactions()
+        except discord.Forbidden:  # cannot remove all reactions
+            for key in controls.keys():
+                await message.remove_reaction(key, ctx.bot.user)
+        except discord.NotFound:
+            return
+    else:
+        return await controls[react.emoji](
+            ctx, pages, controls, message, page, timeout, react.emoji
+        )
+
+
+async def next_page(
+    ctx: commands.Context,
+    pages: list,
+    controls: dict,
+    message: discord.Message,
+    page: int,
+    timeout: float,
+    emoji: str,
+):
+    perms = message.channel.permissions_for(ctx.me)
+    if perms.manage_messages:  # Can manage messages, so remove react
+        with contextlib.suppress(discord.NotFound):
+            await message.remove_reaction(emoji, ctx.author)
+    if page == len(pages) - 1:
+        page = 0  # Loop around to the first item
+    else:
+        page = page + 1
+    return await menu(ctx, pages, controls, message=message, page=page, timeout=timeout)
+
+
+async def prev_page(
+    ctx: commands.Context,
+    pages: list,
+    controls: dict,
+    message: discord.Message,
+    page: int,
+    timeout: float,
+    emoji: str,
+):
+    perms = message.channel.permissions_for(ctx.me)
+    if perms.manage_messages:  # Can manage messages, so remove react
+        with contextlib.suppress(discord.NotFound):
+            await message.remove_reaction(emoji, ctx.author)
+    if page == 0:
+        page = len(pages) - 1  # Loop around to the last item
+    else:
+        page = page - 1
+    return await menu(ctx, pages, controls, message=message, page=page, timeout=timeout)
+
+
+async def close_menu(
+    ctx: commands.Context,
+    pages: list,
+    controls: dict,
+    message: discord.Message,
+    page: int,
+    timeout: float,
+    emoji: str,
+):
+    with contextlib.suppress(discord.NotFound):
+        await message.delete()
+
+
+def start_adding_reactions(
+    message: discord.Message,
+    emojis: Iterable[_ReactableEmoji],
+    loop: Optional[asyncio.AbstractEventLoop] = None,
+) -> asyncio.Task:
+    """Start adding reactions to a message.
+
+    This is a non-blocking operation - calling this will schedule the
+    reactions being added, but the calling code will continue to
+    execute asynchronously. There is no need to await this function.
+
+    This is particularly useful if you wish to start waiting for a
+    reaction whilst the reactions are still being added - in fact,
+    this is exactly what `menu` uses to do that.
+
+    This spawns a `asyncio.Task` object and schedules it on ``loop``.
+    If ``loop`` omitted, the loop will be retrieved with
+    `asyncio.get_event_loop`.
+
+    Parameters
+    ----------
+    message: discord.Message
+        The message to add reactions to.
+    emojis : Iterable[Union[str, discord.Emoji]]
+        The emojis to react to the message with.
+    loop : Optional[asyncio.AbstractEventLoop]
+        The event loop.
+
+    Returns
+    -------
+    asyncio.Task
+        The task for the coroutine adding the reactions.
+
+    """
+
+    async def task():
+        # The task should exit silently if the message is deleted
+        with contextlib.suppress(discord.NotFound):
+            for emoji in emojis:
+                await message.add_reaction(emoji)
+
+    if loop is None:
+        loop = asyncio.get_running_loop()
+    else:
+        warnings.warn(
+            "Explicitly passing the loop will not work in Red 3.4+", DeprecationWarning
+        )
+
+    return loop.create_task(task())
+
+
+DEFAULT_CONTROLS = {"⬅": prev_page, "❌": close_menu, "➡": next_page}
+
+# Buttons and other UI stuff below
+
+class ConfirmButton(discord.ui.View):
+    """
+    Simple Confirm Button
+
+    example:
+        view = ConfirmButton()
+        await ctx.send('Do you want to continue?', view=view)
+        await view.wait()
+        if view.value is None:
+            print('Timed out...')
+        elif view.value:
+            print('Confirmed...')
+        else:
+            print('Cancelled...')
+    """
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
+    async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
+        await interaction.response.send_message('Confirming', ephemeral=True)
+        self.value = True
+        self.stop()
+
+    # This one is similar to the confirmation button except sets the inner value to `False`
+    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
+    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        await interaction.response.send_message('Cancelling', ephemeral=True)
+        self.value = False
+        self.stop()
+
+class CounterButton(discord.ui.View):
+    """
+    Creates a simple counter button that stops when it hits 5.
+    Upon stopping it greys out.
+
+    Example:
+        await ctx.send('Press!', view=Counter())
+    """
+    @discord.ui.button(label='0', style=discord.ButtonStyle.red)
+    async def count(self, button: discord.ui.Button, interaction: discord.Interaction):
+        number = int(button.label) if button.label else 0
+        if number + 1 >= 5:
+            button.style = discord.ButtonStyle.green
+            button.disabled = True
+        button.label = str(number + 1)
+
+        # Make sure to update the message with our updated selves
+        await interaction.response.edit_message(view=self)
+
+class Dropdown(discord.ui.Select):
+    """
+    Simple Dropdown menu
+    Can obviously be edited for needs,
+    Maybe usage in music?
+
+    Example:
+        view = DropdownView() # See class below.
+        await ctx.send('Pick your favourite colour:', view=view)
+    """
+    def __init__(self):
+
+        # Set the options that will be presented inside the dropdown
+        options = [
+            discord.SelectOption(label='Red', description='Your favourite colour is red', emoji='🟥'),
+            discord.SelectOption(label='Green', description='Your favourite colour is green', emoji='🟩'),
+            discord.SelectOption(label='Blue', description='Your favourite colour is blue', emoji='🟦')
+        ]
+
+        # The placeholder is what will be shown when no option is chosen
+        # The min and max values indicate we can only pick one of the three options
+        # The options parameter defines the dropdown options. We defined this above
+        super().__init__(placeholder='Choose your favourite colour...', min_values=1, max_values=1, options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        # Use the interaction object to send a response message containing
+        # the user's favourite colour or choice. The self object refers to the
+        # Select object, and the values attribute gets a list of the user's
+        # selected options. We only want the first one.
+        await interaction.response.send_message(f'Your favourite colour is {self.values[0]}')
+
+
+class DropdownView(discord.ui.View):
+    """
+    Helper for dropdown
+    """
+    def __init__(self):
+        super().__init__()
+
+        # Adds the dropdown to our view object.
+        self.add_item(Dropdown())
