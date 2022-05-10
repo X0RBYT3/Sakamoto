206c76d2af76229888781563fae2f2372bcd276d
Nekurone <zx627924@ou.ac.uk>
2022-05-10 04:40:17 +0100

    Return error message on cog not loading

--- src/core/cogmanager.py
+++ src/core/cogmanager.py
@@ -23,6 +23,7 @@ async def cogs_manager(client: discord.Client, mode: str, cogs: list[str]) -> No
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno, exc)
             print(f"**`ERROR:`** Cog Loading Failed: {cog}\n{exc}")
+            return f"**`ERROR:`** Cog Loading Failed: {cog}\n{exc}"
         else:
             print(f"**`SUCCESS`** - {cog} {mode}ed! \N{PISTOL}")
         # Since we only need this for sending to discord
